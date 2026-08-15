dd02e68bf81a169562ebcfa1e5f231b65a2ba0fd
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-14 23:12:31 -0300

    feat: implementa logica de pagamento cartao de credito

--- src/main/java/SistemaDePagamento/CartaoCredito.java
+++ src/main/java/SistemaDePagamento/CartaoCredito.java
@@ -4,6 +4,10 @@ public class CartaoCredito extends FormaPagamento {
 
     @Override
     public void processarPagamento(double valor) {
+        double taxa = valor * 0.05;
+        double valorFinal = valor + taxa;
         System.out.print("Processando R$" + valor + " no Cartão de Crédito. Cobrando taxa de 5%.");
+        System.out.println("Taxa (5%): R$ " + taxa);
+        System.out.println("Total a pagar: R$ " + valorFinal);
     }
 }
