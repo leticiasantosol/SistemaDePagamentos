a46e19b5fb90e07798451ede3abec9274480e5aa
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-14 23:14:30 -0300

    feat: implementa logica de pagamento pix

--- src/main/java/SistemaDePagamento/Pix.java
+++ src/main/java/SistemaDePagamento/Pix.java
@@ -4,7 +4,11 @@ public class Pix extends FormaPagamento{
 
     @Override
     public void processarPagamento(double valor) {
+        double desconto = valor * 0.10;
+        double valorFinal = valor - desconto;
         System.out.print("Processando R$" + valor + " via PIX. Desconto de 10$ aplicado!");
+        System.out.println("Taxa (5%): R$ " + desconto);
+        System.out.println("Total a pagar: R$ " + valorFinal);
     }
 
 }
