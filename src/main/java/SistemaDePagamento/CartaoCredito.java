43be7873a1fd28a88ef8341dc14760c0ad6ebfb2
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-14 23:04:02 -0300

    feat: implementa metodo processarPagamento cartao de credito

--- src/main/java/SistemaDePagamento/CartaoCredito.java
+++ src/main/java/SistemaDePagamento/CartaoCredito.java
@@ -2,4 +2,8 @@ package SistemaDePagamento;
 
 public class CartaoCredito extends FormaPagamento {
 
+    @Override
+    public void processarPagamento(double valor) {
+        System.out.print("Processando R$" + valor + " no Cartão de Crédito. Cobrando taxa de 5%.");
+    }
 }
