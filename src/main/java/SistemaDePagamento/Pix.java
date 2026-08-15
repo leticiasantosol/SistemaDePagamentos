54d8730f8c2cf70edd0cf6149b6f5f261e93e609
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-14 23:09:30 -0300

    feat: implementa metodo processarPagamento pix

--- src/main/java/SistemaDePagamento/Pix.java
+++ src/main/java/SistemaDePagamento/Pix.java
@@ -2,5 +2,9 @@ package SistemaDePagamento;
 
 public class Pix extends FormaPagamento{
 
+    @Override
+    public void processarPagamento(double valor) {
+        System.out.print("Processando R$" + valor + " via PIX. Desconto de 10$ aplicado!");
+    }
 
 }
